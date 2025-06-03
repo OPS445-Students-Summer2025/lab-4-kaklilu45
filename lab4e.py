8e92d0e2cfffc642074699b96f2196c83397c102
Kerubel Aklilu <kaklilu@myseneca.ca>
2025-06-02 20:22:41 -0400

    added lab4e.py

---
+++ lab4e.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Author ID: 111658217
+
+def is_digits(sobj):
+    # place code here - loop through each character in sobj 
+    letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+    
+    for char in sobj:
+        if char in letters:
+            return False
+    return True
+
+if __name__ == '__main__':
+    test_list = ['x3058','3058','8503x','8503']
+    for item in test_list:
+        if is_digits(item):
+            print(item,'is an integer.')
+        else:
+            print(item,'is not an integer.')
